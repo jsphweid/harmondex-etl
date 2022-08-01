32c0aace3aeb2dd4de78d1bb346cb2fcce53b2eb
Joseph Weidinger <jsphweid@gmail.com>
2022-08-01 17:06:22 -0500

    move import to runtime

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 
-from etl import lmd
-
-
 if __name__ == "__main__":
     if not os.getenv("STORAGE_PATH"):
         raise Exception("STORAGE_PATH env var is required!")
+
+    from etl import lmd
+
     lmd.process()
